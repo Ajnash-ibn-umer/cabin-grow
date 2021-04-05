daf049fba80e404e50d77b1842a852f37ba1a585
ajnash-aju <shanu.kolakkadan@gmail.com>
2021-04-05 17:58:48 +0530

    image compressed animation  changed hover bug fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,16 @@ console.log('sss');
 $(document).ready(() => {
 
         console.log('enter');
-        $("#wlcm").fadeIn(3000)
-        $("#readmore").fadeIn(3000)
-        $("#abtus").fadeIn(5000)
+        $("#wlcm").slideToggle(1000)
+        $("#readmore").slideToggle(1000)
+        $("#abtus").slideToggle(1000)
 
 
         /* $(".readmore-btn").mouseenter(function () {
                  console.log("enter");
                  $(this).css({ "background-color": "rgba(0, 0, 0, 0.391)"})
          });*/
-        $(".readmore-btn").mouseleave(function () {
-                console.log("enter");
-                $(this).css({ "background-color": "rgba(0, 0, 0, 0)" })
-        });
+       
 
 
 
@@ -23,27 +20,7 @@ $(document).ready(() => {
 
 
 
-
-        var isrotated = false
-        $(".cards").click(function (e) {
-
-
-                if (isrotated) {
-
-
-                        console.log("second" + isrotated);
-
-                        $(this).css({ 'transform': 'rotate(0deg)', "width": "10rem" });
-
-                        isrotated = false
-
-                } else if (!isrotated) {
-                        isrotated = true;
-                        console.log("first" + isrotated);
-
-                        $(this).css({ 'transform': 'rotate(10deg)', "width": "10rem" });
-                }
-        });
+       
 
 
 })
